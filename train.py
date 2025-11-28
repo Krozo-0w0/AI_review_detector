39e253db5a176050c8985927f920560e406ec458
krozo <158836210+Krozo-0w0@users.noreply.github.com>
2025-11-28 18:42:08 +0800

    Data cleaning
    - Added data cleaning before training

--- train.py
+++ train.py
@@ -9,38 +9,66 @@ from sklearn.metrics import accuracy_score
 
 from sklearn.model_selection import train_test_split
 
-#For training the model 
-df = pd.read_csv('fake reviews dataset.csv')
+def clean_data(df):
+    print("Cleaning Data.")
 
-X = df[["category", "rating", "text_"]]
-y = df['label']
+    # Check the data types
+    print("Data Types:")
+    print(df.dtypes)
 
-X_train, X_test, y_train, y_test =  train_test_split(X, y)
+    # Check for missing values in each column
+    print("\nMissing Values:")
+    print(df.isnull().sum())
 
-# Combine X and y for saving
-train_df = pd.concat([X_train, y_train], axis=1)
-test_df  = pd.concat([X_test, y_test], axis=1)
+    # Drop rows with any missing values
+    df_clean = df.dropna()
 
-# Save to CSV
-train_df.to_csv("train_data.csv", index=False)
-test_df.to_csv("test_data.csv", index=False)
+    # Verify that missing rows are removed
+    print("\nMissing Values After Cleaning:")
+    print(df_clean.isnull().sum())
+    print("\nData processing done!")
+    print("====================================")
+    return df_clean
 
-preprocess = ColumnTransformer(
-    transformers=[
-        ("cat", OneHotEncoder(handle_unknown="ignore"), ["category"]),
-        ("text", TfidfVectorizer(), "text_"),
-        ("num", "passthrough", ["rating"])
-    ]
-)
+def main():
+    #For training the model 
+    df = pd.read_csv('fake reviews dataset.csv')
 
-model = Pipeline([
-    ("preprocess", preprocess),
-    ("svm", SVC(kernel="linear"))
-])
+    X = df[["category", "rating", "text_"]]
+    y = df['label']
 
-model.fit(X_train, y_train)
+    df = clean_data(df)
 
-joblib.dump(model, "model.pkl") 
+    X_train, X_test, y_train, y_test =  train_test_split(X, y)
 
-print("Model Done Training. Exported to \"model.pkl\"")
+    # Combine X and y for saving
+    train_df = pd.concat([X_train, y_train], axis=1)
+    test_df  = pd.concat([X_test, y_test], axis=1)
 
+    # Save to CSV
+    train_df.to_csv("train_data.csv", index=False)
+    test_df.to_csv("test_data.csv", index=False)
+
+    preprocess = ColumnTransformer(
+        transformers=[
+            ("cat", OneHotEncoder(handle_unknown="ignore"), ["category"]),
+            ("text", TfidfVectorizer(), "text_"),
+            ("num", "passthrough", ["rating"])
+        ]
+    )
+
+    model = Pipeline([
+        ("preprocess", preprocess),
+        ("svm", SVC(kernel="linear"))
+    ])
+    print("====================================")
+    print("Started Model Training!")
+    
+    model.fit(X_train, y_train)
+
+    joblib.dump(model, "model.pkl") 
+
+    print("Model Done Training. Exported to \"model.pkl\"")
+
+if __name__ == "__main__":
+    main()
